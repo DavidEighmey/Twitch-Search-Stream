a82a67faf5cad87cb7df420bba224ec4687c45d6
David Eighmey <deighmey11@gmail.com>
2015-09-22 14:32:28 -0700

    initial commit

---
+++ custom.js
@@ -0,0 +1,55 @@
+
+function searchFunction() {
+    var x = document.getElementById("mySearch").value;
+ 	console.log(x);
+ 	var url = "";
+    url = "https://api.twitch.tv/kraken/search/streams?q=" + x;
+	var xmlhttp = new XMLHttpRequest();
+
+
+	xmlhttp.onreadystatechange = function() {
+    	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        	var myArr = JSON.parse(xmlhttp.responseText);
+        	console.log(myArr)
+        	myFunction(myArr);
+    	}
+	}
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
+
+
+	function myFunction(arr) {
+		var array = arr.streams
+    	var out = "";
+    	var i;
+
+    	document.getElementById("results").innerHTML = "Total results: " + arr._total;
+    	document.getElementById("paging").innerHTML = '<button type="button" class="btn btn-default" aria-label="Left Align"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span></button>';
+		document.getElementById("paging").innerHTML += '1/' + Math.floor(arr._total/10) + 'pages';
+		document.getElementById("paging").innerHTML += '<button type="button" class="btn btn-default" aria-label="Left Align"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span></button>';
+
+
+    	for(i = 0; i < array.length; i++) {
+    		out += '<div>';
+    		out += '<a href="' + array[i].channel.url + '" target="_blank"><img src="' + array[i].preview.medium + '" class="img-thumbnail imgPad" id="thumbnail" alt="Cinque Terre" width="304" height="236"></a>';
+    		out += '<h2>' + array[i].channel.display_name + '</h2>';
+    		out += '<div><p>' + array[i].game + ' - ' + array[i].viewers + ' viewers</p></div>';
+    		out += '<div><p>' + array[i].channel.status  + '</p></div>';
+        	out += '<br><br><br>';
+        	out += '</div>';
+        	out += '<br>';
+    	}
+    	document.getElementById("streams").innerHTML = out;
+	}
+}
+
+/*
+	<div>
+    	<img src="cinqueterre.jpg" class="img-thumbnail" id="thumbnail" alt="Cinque Terre" width="304" height="236">
+    	<div>
+	    	<h2>Stream Display Name</h2>
+	  		<div><p>Game name - # of views </p></div>
+	  		<div><p>Description</p></div>
+  		</div>
+    </div>
+*/
